dda394db3c24c0e6f2eb0f57dacf84c8dd2034b1
Jordi <jordilop@gmail.com>
2023-01-29 17:47:34 +0100

    added styles in BookDetail component

--- src/components/BookDetail/BookDetail.js
+++ src/components/BookDetail/BookDetail.js
@@ -4,8 +4,8 @@ import { useParams } from "react-router-dom";
 import { getBookDetails } from "../../services/books";
 import parse from "html-react-parser";
 import Loading from "../Loading/Loading";
-import { Col, Container, Row } from "react-bootstrap";
-
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { FaAmazon } from "react-icons/fa";
 
 function BookDetail() {
 
@@ -17,13 +17,11 @@ function BookDetail() {
 
     const date = new Date(data.publishedDate);
 
-    // const dateFormat = new Intl.DateTimeFormat('es-ES', { dateStyle: 'medium' }).format(date);
-
     useEffect(() => {
         setLoading(true);
         getBookDetails(bookId)
             .then(response => {
-                console.log(response.data.volumeInfo);
+                // console.log(response.data.volumeInfo);
                 setData(response.data.volumeInfo);
                 setLoading(false);
             })
@@ -38,42 +36,54 @@ function BookDetail() {
     if (loading) return <Loading />
 
     return (
-        <Container>
-            <h4 className="text-center mb-5">{data.title}</h4>
-            <Row className="mb-4 border-bottom">
-                <Col lg={2} md={3} className="text-center">
-                    <img src={data.imageLinks?.thumbnail} alt={data.title} />
-                    <p className="mt-1">Rating: {data.averageRating}</p>
-                    <p>({data.ratingsCount} ratings)</p>
-                </Col>
-                <Col>
-                    <p className="text-primary">{data.authors}</p>
-                    <p className="text-muted">{data.publisher}, {data.publishedDate} - {data.pageCount} pages</p>
-                    <p>{parse('' + data.description)}</p>
-                </Col>
-            </Row>
-            <h5>Info biblio</h5>
-            <Row>
-                <Col md={2}>Title</Col>
-                <Col>{data.title}</Col>
-            </Row>
-            <Row>
-                <Col md={2}>Author</Col>
-                <Col>{data.authors}</Col>
-            </Row>
-            <Row>
-                <Col md={2}>Editor</Col>
-                <Col>{data.publisher}, {date.getFullYear()}</Col>
-            </Row>
-            <Row>
-                <Col md={2}>ISBN</Col>
-                <Col>{data.industryIdentifiers[0].identifier}</Col>
-            </Row>
-            <Row>
-                <Col md={2}>Num. Pages</Col>
-                <Col>{data.pageCount}</Col>
-            </Row>
-        </Container>
+        <>
+            <Container>
+                <h4 className="text-center mb-5">{data.title}</h4>
+                <Row className="mb-4 border-bottom">
+                    <Col lg={2} md={3} className="text-center">
+                        <img src={data.imageLinks?.thumbnail} alt={data.title} />
+                        <p className="mt-1 mb-0">Puntuación: {data.averageRating ? data.averageRating : "N/A"}</p>
+                        <p>({data.ratingsCount ? data.ratingsCount : 0} reseñas)</p>
+                    </Col>
+                    <Col>
+                        <p className="text-primary mb-1">{data.authors}</p>
+                        <p className="text-muted">{data.publisher}, {date.getFullYear()} - {data.pageCount} pages</p>
+                        <p>{parse('' + data.description)}</p>
+                    </Col>
+                </Row>
+                <Row className="pb-4 border-bottom">
+                    <h5>Información bibliográfica</h5>
+                    <Row className="mb-2 mb-md-0">
+                        <Col className="fst-italic" md={3} lg={2}>Título</Col>
+                        <Col>{data.title}</Col>
+                    </Row>
+                    <Row className="mb-2 mb-md-0">
+                        <Col className="fst-italic" md={3} lg={2}>Autor</Col>
+                        <Col>{data.authors}</Col>
+                    </Row>
+                    <Row className="mb-2 mb-md-0">
+                        <Col className="fst-italic" md={3} lg={2}>Editor</Col>
+                        <Col>{data.publisher}, {date.getFullYear()}</Col>
+                    </Row>
+                    <Row className="mb-2 mb-md-0">
+                        <Col className="fst-italic" md={3} lg={2}>ISBN</Col>
+                        <Col>{data.industryIdentifiers?.map(isbn => isbn.identifier)?.join(', ')}</Col>
+                    </Row>
+                    <Row className="mb-2 mb-md-0">
+                        <Col className="fst-italic" md={3} lg={2}>N.º de páginas</Col>
+                        <Col>{data.pageCount}</Col>
+                    </Row>
+                </Row>
+                <Row className=" justify-content-center">
+                    <Col md={4} className="d-grid mx-auto my-4">
+                        <Button variant="info" onClick={() => window.open(`https://www.amazon.es/dp/${data.industryIdentifiers[0]?.identifier}`)}>
+                            <FaAmazon className="me-2" />
+                            Comprar
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
 
